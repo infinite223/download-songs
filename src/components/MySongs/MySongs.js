f75014b97df8c6a5524e6494b7ad54428afeda39
infinite223 <dawidszmigiel9@gmail.com>
2022-02-20 23:38:58 +0100

    better slider add songs

--- src/components/MySongs/MySongs.js
+++ src/components/MySongs/MySongs.js
@@ -1,46 +1,40 @@
-import React, { useState } from 'react'
+import React, { useRef,useState,useEffect } from 'react'
 import './MySongs.css'
 import Card from '../Card';
 import {DownloadSong} from './../DataApi/DownloadSong.js'
-import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
+import {motion} from 'framer-motion'
 const MySongs = ({ mySongs, deleteSong }) => {
   const [toggleSongs, setToggleSongs] = useState(false);
-  const [current, setCurrent] = useState(0);
-  const length = mySongs.length;
-
-  const nextSong = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
-  };
-
-  const prevSong = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
-  };
-  console.log(current)
+  const [width, setWidth] = useState(0);
+  const mySongs_list = useRef();
+   
+  useEffect(() =>{
+    if(toggleSongs)
+    {
+      setWidth(mySongs_list.current.scrollWidth - mySongs_list.current.offsetWidth);
+      console.log(width)
+    }
+  }, [toggleSongs]);
+  //jakies domyślne wartości dać itp
   return (
     <>
-      <div className='mySongs' onClick={()=>setToggleSongs(true)}>My Songs</div>
+      <div className='mySongs'  onClick={()=>setToggleSongs(true)}>My Songs</div>
       {toggleSongs&&
-        <div className='mySongs_list'>
-          <FaArrowAltCircleLeft className='left-arrow' onClick={prevSong} />
-          <FaArrowAltCircleRight className='right-arrow' onClick={nextSong} />
+        <motion.div ref={mySongs_list} className='mySongs_list'>        
           <div className='mySongs_list_title'>
-            <h2>Lista dodanych piosenek...</h2>
-            <text onClick={()=>setToggleSongs(false)}>Close</text>
+            <motion.h2 >Lista dodanych piosenek:</motion.h2>
+            <text onClick={()=>setToggleSongs(false)}>X</text>
           </div>
-          <div className='mySongs_list_songs'>
-            {mySongs.map((item,i) => {
-              return(
-              <>
-                {i === current && (
-                  <div className='mySongs_list_songs_card'>
+          <motion.div className='mySongs_list_songs' drag="x" dragConstraints={{right:0, left:-width}}>
+            {mySongs.map((item,i) => { 
+            return (             
+                  <motion.div className='mySongs_list_songs_card'>
                     <Card Songs={deleteSong} key={item.id.videoId} number={i+1} item={item}></Card>
-                    <div className='mySongs_download' onClick={()=> DownloadSong(item.id.videoId)}>Download</div>
-                  </div>
-                )}
-              </>
-            )})}
-           </div>          
-        </div>
+                    <motion.div className='mySongs_download' onClick={()=> DownloadSong(item.id.videoId)}>Download</motion.div>
+                  </motion.div>                         
+               )})}
+           </motion.div>          
+        </motion.div>
       }
     </>
   )
